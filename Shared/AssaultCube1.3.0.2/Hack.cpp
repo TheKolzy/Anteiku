3b2a900c2a7f1292cb46bdd7383be0ab3912a8ee
TheKolzy <kolzykun@gmail.com>
2025-03-27 10:33:36 +0100

    Implementation of initializePlayerList

--- Shared/AssaultCube1.3.0.2/Hack.cpp
+++ Shared/AssaultCube1.3.0.2/Hack.cpp
@@ -5,8 +5,29 @@ try
 	: m_process { processName }, m_memory { m_process.getIdentifier() }
 	, m_player { m_process.getAddress() + static_cast<std::uintptr_t>(Offsets::g_player) }
 {
+	initializePlayerList();
 }
 catch (const std::exception& exception) {
     std::println("{}", exception.what());
     std::exit(EXIT_FAILURE);
+}
+
+void Hack::run() noexcept
+{
+}
+
+void Hack::initializePlayerList() noexcept
+{
+	m_playerList.clear();
+
+	int playerCount { Memory::read<int>(m_process.getAddress() + Offsets::g_playerCount) };
+	std::uintptr_t addressPlayerList { Memory::read<std::uintptr_t>
+		(m_process.getAddress() + Offsets::g_playerList) };
+	addressPlayerList += 0x4;
+
+	for (int i {}; i < playerCount - 1; ++i)
+	{
+		m_playerList.push_back(Player { addressPlayerList });
+		addressPlayerList += 0x4;
+	}
 }
--- Shared/AssaultCube1.3.0.2/Hack.h
+++ Shared/AssaultCube1.3.0.2/Hack.h
@@ -6,19 +6,25 @@
 #include "Player.h"
 #include "Process.h"
 
+#include <cstdint>
 #include <print>
 #include <string_view>
+#include <vector>
 
 class Hack
 {
 public:
 	explicit Hack(std::wstring_view processName);
+	void run() noexcept;
 
 private:
-	const Process m_process;
-	const Memory  m_memory;
+	void initializePlayerList() noexcept;
 
-	const Player  m_player;
+	const Process       m_process;
+	const Memory        m_memory;
+
+	const Player        m_player;
+	std::vector<Player> m_playerList {};
 };
 
 #endif
